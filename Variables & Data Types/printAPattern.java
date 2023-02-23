c3c5c102c531723d4084d92c2cbc4964ee6b2d3b
Rahul Jangid <rahuljangid236@gmail.com>
2023-02-24 00:43:32 +0530

    24-02-2023

---
+++ Variables & Data Types/printAPattern.java
@@ -0,0 +1,9 @@
+public class printAPattern {
+    public static void main(String args[]) {
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
