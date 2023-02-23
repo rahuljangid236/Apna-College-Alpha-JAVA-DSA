a1ac9daef0ad172092a1a9086924881af34ecda4
Rahul Jangid <rahuljangid236@gmail.com>
2023-02-24 01:05:19 +0530

    24-02-2023

---
+++ Variables & Data Types/productOfAandB.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class productOfAandB {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int product = a * b;
+        System.out.print(product);
+    }
+}
---
+++ Variables & Data Types/sumOfAandB.java
@@ -0,0 +1,9 @@
+public class sumOfAandB {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 55;
+        int sum = a + b;
+        System.out.print(sum);
+
+    }
+}
