62e1ee8575f5ff8ecab9863f0d8ef85dc4846555
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-01 00:15:46 +0530

    01-03-2023

---
+++ Pattern/half_pramid.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class half_pramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
