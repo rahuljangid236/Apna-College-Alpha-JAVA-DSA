cf7e12e87e48e8be35a7b1d2458ccd1c76d28a92
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-03 19:39:56 +0530

    Update factorial.java

--- Functino & Method/factorial.java
+++ Functino & Method/factorial.java
@@ -1,10 +1,14 @@
 
 public class factorial {
     public static int factorial(int n) {
-
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(factorial(4));
     }
 }
