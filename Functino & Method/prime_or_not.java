2baca95338bc46ce8da2c4ca6193768fa34a4258
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-05 02:25:05 +0530

    Create prime_or_not.java

---
+++ Functino & Method/prime_or_not.java
@@ -0,0 +1,17 @@
+public class prime_or_not {
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(10));
+    }
+}
