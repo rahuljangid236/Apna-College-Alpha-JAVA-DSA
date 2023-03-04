91da942826d904b8f5537e460dbc7aa3efdd552e
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-05 02:39:01 +0530

    Create primes_in_range.java

---
+++ Functino & Method/primes_in_range.java
@@ -0,0 +1,26 @@
+public class primes_in_range {
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void primesInRange(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        primesInRange(20);
+    }
+}
