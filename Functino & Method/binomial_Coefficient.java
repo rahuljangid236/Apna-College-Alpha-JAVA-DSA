5830b5f73de31f722687b592953c7a4ac088c943
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-05 00:14:12 +0530

    Update binomial_Coefficient.java

--- Functino & Method/binomial_Coefficient.java
+++ Functino & Method/binomial_Coefficient.java
@@ -1,3 +1,22 @@
 public class binomial_Coefficient {
+    public static int findFactorial(int n) {
+        int f = 1;
+        for (int i = 1; i <= n; i++) {
+            f = f * i;
+        }
+        return f;
+    }
 
+    public static int binCoeff(int n, int r) {
+        int fact_n = findFactorial(n);
+        int fact_r = findFactorial(r);
+        int fact_nmr = findFactorial(n - r);
+
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+        return binCoeff;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(binCoeff(5, 2));
+    }
 }
