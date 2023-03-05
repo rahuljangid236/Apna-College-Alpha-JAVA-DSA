4cc22cbc10f521fc349c960bd336454af098e7fd
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-06 00:53:54 +0530

    Create largestInArray.java

---
+++ Array/largestInArray.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class largestInArray {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < numbers.length; i++) {
+            if (largest < numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+        return largest;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 1, 2, 6, 3, 5 };
+        System.out.println("Largest numbers is : " + getLargest(numbers));
+
+    }
+}
