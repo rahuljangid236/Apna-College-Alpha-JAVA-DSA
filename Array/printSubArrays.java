57b583add0005cabfc90338f98782a68ac5c7ed2
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-08 00:29:12 +0530

    Create printSubArrays.java

---
+++ Array/printSubArrays.java
@@ -0,0 +1,20 @@
+package Array;
+
+public class printSubArrays {
+    public static void printSubArray(int numbers[]) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(numbers[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { 2, 4, 6, 8, 10 };
+        printSubArray(numbers);
+    }
+}
