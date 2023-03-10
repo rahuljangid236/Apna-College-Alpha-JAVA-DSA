64396a77e50d011fb2776d843b464c06bc480068
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-11 00:26:05 +0530

    Create trappingRainWater.java

---
+++ Array/trappingRainWater.java
@@ -0,0 +1,31 @@
+package Array;
+
+public class trappingRainWater {
+    public static int trappedRainWater(int height[]) {
+        int n = height.length;
+        // Calculate left max boundary - Array
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        // Calculate right max boundary - Array
+        int rightMax[] = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        // loop
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) {
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+            trappedWater += waterLevel - height[i];
+        }
+        return trappedWater;
+    }
+
+    public static void main(String[] args) {
+        int height[] = { 4, 2, 0, 6, 3, 2, 5 };
+        System.out.println(trappedRainWater(height));
+    }
+}
