d02797edb3b4ec98060950bcf3a9bc329b416499
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-11 00:54:38 +0530

    Create buSellStock.java

---
+++ Array/buSellStock.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class buSellStock {
+    public static int buySellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buySellStocks(prices));
+    }
+}
