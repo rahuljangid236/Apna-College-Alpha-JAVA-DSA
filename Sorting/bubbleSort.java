5fcb9f26e8bf1fce3c3cc72180a06074e5f0c424
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-13 00:02:32 +0530

    Create bubbleSort.java

---
+++ Sorting/bubbleSort.java
@@ -0,0 +1,28 @@
+package Sorting;
+
+public class bubbleSort {
+    public static void arraySort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        arraySort(arr);
+        printArray(arr);
+    }
+}
