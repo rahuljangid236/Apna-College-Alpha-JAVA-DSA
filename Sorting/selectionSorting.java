55ef198fd039109a7c5958ac4b1be8b6e96e73cb
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-13 00:24:56 +0530

    Create selectionSorting.java

---
+++ Sorting/selectionSorting.java
@@ -0,0 +1,30 @@
+package Sorting;
+
+public class selectionSorting {
+    public static void selectionSort(int arr[]) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
+                }
+            }
+            // Swap
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        selectionSort(arr);
+        printArray(arr);
+    }
+}
