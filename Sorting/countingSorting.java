202d7ecc7326e13fb1979836ac70985f5cccb594
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-16 23:28:03 +0530

    Create countingSorting.java

---
+++ Sorting/countingSorting.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+public class countingSorting {
+    public static void countingSort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        // Sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 };
+        countingSort(arr);
+        printArray(arr);
+    }
+}
