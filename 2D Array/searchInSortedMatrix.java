95fc91f4790d0bf4c50f9b5bb159136b52bfcf1d
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-18 21:53:07 +0530

    Update searchInSortedMatrix.java

--- 2D Array/searchInSortedMatrix.java
+++ 2D Array/searchInSortedMatrix.java
@@ -1,2 +1,28 @@
 public class searchInSortedMatrix {
+    public static boolean stairCaseSearch(int matrix[][],int key){
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row < matrix.length && col >=0){
+            if(matrix[row][col] == key){
+                System.out.println("Found Key at (" + row + "," + col + ")");
+                return true;
+            }else if(key < matrix[row][col]){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        System.out.println("Key not found");
+        return false;
+    }
+    public static void main(String[] args) {
+        int matrix[][] = {
+                {10,20,30,40},
+                {15,25,35,45},
+                {27,29,37,48},
+                {32,33,39,50}
+        };
+        int key = 33;
+        stairCaseSearch(matrix,key);
+    }
 }
