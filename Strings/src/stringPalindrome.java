61099a6bc02b8afb37a3c2598441e362b86aa7fb
Rahul Jangid <rahuljangid236@gmail.com>
2023-03-19 00:28:14 +0530

    Update stringPalindrome.java

--- Strings/src/stringPalindrome.java
+++ Strings/src/stringPalindrome.java
@@ -1,5 +1,15 @@
 public class stringPalindrome {
+    public static boolean palindrome(String str){
+        for(int i=0; i<str.length()/2; i++){
+            int n = str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        
+        String str = "racecar";
+        System.out.println(palindrome(str));
     }
 }
